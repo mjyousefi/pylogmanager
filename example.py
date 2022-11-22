d750ca69f6f897da3cc2c99aa18c44f45998186b
MJ Yousefi <mjyousefi10@gmail.com>
2022-11-22 14:14:04 +0330

    update example.py
    debug and exception logs are added.

--- example.py
+++ example.py
@@ -1,3 +1,4 @@
+#Utils File
 from log_manager import LogManager
 from dotenv import load_dotenv
 import os
@@ -14,3 +15,25 @@ class LogManagerUtils():
         debug_log_is_enabled = os.getenv('ELASTIC_LOG_DEBUG_ENABLED')
         info_log_is_enabled = os.getenv('ELASTIC_LOG_INFO_ENABLED')
         return LogManager(elastic_host, elastic_default_index_prefix, debug_log_is_enabled, info_log_is_enabled)
+    
+    
+#Craete Log    
+from log_manager_utils import LogManagerUtils
+
+app_log_manager = LogManagerUtils.get_log_manager()
+
+
+#log debug
+app_log_manager.log_debug('ExampleLog', action_name='TestLog')
+
+
+#log exception
+try:
+    content = a / 2
+except Exception as e:
+    if hasattr(e, 'message'):
+        exception_message = repr(e.message0)
+    else:
+        exception_message = repr(e)
+    message={"Exception":exception_message}
+    app_log_manager.log_exception(message, action_name='APICalled')
